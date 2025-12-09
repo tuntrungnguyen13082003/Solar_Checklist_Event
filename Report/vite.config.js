66a5ccc00f00ab0f652514caad8c16bae94fa5bb
tuntrungnguyen13082003 <tuntrungnguyen@gmail.com>
2025-12-09 09:05:59 +0700

    Configure asset file names in Vite build
    Added build configuration for asset file naming.

--- Report/vite.config.js
+++ Report/vite.config.js
@@ -6,4 +6,23 @@ export default defineConfig({
   plugins: [react()],
   // ĐÃ XÓA DÒNG BASE
   base: '/Solar_Checklist_Event/',
+    build: {
+    rollupOptions: {
+      output: {
+        // Dòng này giúp giữ nguyên tên file trong thư mục assets
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.')[1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          // Cấu hình tên file: assets/tên-gốc.đuôi
+          return `assets/[name][extname]`;
+        },
+        
+        // Tương tự cho file JS và CSS (nếu muốn)
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js',
+      },
+    },
+  },
 })
